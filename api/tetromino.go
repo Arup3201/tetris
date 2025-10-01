936af5d24278b46882a9196ed47fb22b0c36a601
Arup3201 <arupjana7365@gmail.com>
2025-10-01 21:27:57 +0530

    seperate square implementation

--- api/tetromino.go
+++ api/tetromino.go
@@ -1,50 +1,5 @@
 package api
 
-type coord struct {
-	X, Y int
-}
-
-type color struct {
-	R, G, B, A uint8
-}
-
-var (
-	COLOR_ORANGE = color{
-		R: 255,
-		G: 165,
-		B: 0,
-		A: 255,
-	}
-)
-
-type Square struct {
-	position coord
-	Color    color
-}
-
-func createSquare(x, y int, color color) *Square {
-	return &Square{
-		position: coord{
-			X: x,
-			Y: y,
-		},
-		Color: color,
-	}
-}
-
-func (sq *Square) changePosition(x, y int) {
-	sq.position.X = x
-	sq.position.Y = y
-}
-
-func (sq *Square) GetX() int {
-	return sq.position.X
-}
-
-func (sq *Square) GetY() int {
-	return sq.position.Y
-}
-
 type Tetromino struct {
 	squares               [4]*Square
 	gridRows, gridColumns int
