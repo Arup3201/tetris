271bc838d334c27d1ccdeeea33a4cd02d78f1193
Arup3201 <arupjana7365@gmail.com>
2025-10-24 11:34:44 +0530

    tetrimino property private

--- api/tetrimino/Ishape.go
+++ api/tetrimino/Ishape.go
@@ -5,25 +5,28 @@ import "github.com/Arup3201/tetris/api/matrix"
 type MatrixSetFunc func(r, c int)
 
 type ITetrimino struct {
-	BoundingBox [4][4]uint8
-	Name        string
+	boundingBox [4][4]uint8
+	name        string
+	top, left   int
 }
 
 func CreateITetrimino(name string) *ITetrimino {
 	return &ITetrimino{
-		BoundingBox: [4][4]uint8{
+		boundingBox: [4][4]uint8{
 			{0, 0, 0, 0},
 			{1, 1, 1, 1},
 			{0, 0, 0, 0},
 			{0, 0, 0, 0},
 		},
-		Name: name,
+		name: name,
+		top:  22,
+		left: 4,
 	}
 }
 
 func (t *ITetrimino) Spawn(matrix *matrix.ReverseMatrix) {
-	matrix.Set(21, 4, t.Name)
-	matrix.Set(21, 5, t.Name)
-	matrix.Set(21, 6, t.Name)
-	matrix.Set(21, 7, t.Name)
+	matrix.Set(t.top-1, t.left, t.name)
+	matrix.Set(t.top-1, t.left+1, t.name)
+	matrix.Set(t.top-1, t.left+2, t.name)
+	matrix.Set(t.top-1, t.left+3, t.name)
 }
