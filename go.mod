e7a7c51ad340904032ce9d0f2ed55dc2c4d27e6e
Arup3201 <arupjana7365@gmail.com>
2025-10-28 10:00:23 +0530

    tetris gui stack and game over success

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/Arup3201/tetris
 go 1.24.5
 
 require (
+	github.com/hajimehoshi/ebiten v1.12.13
 	github.com/hajimehoshi/ebiten/v2 v2.8.8
 	github.com/stretchr/testify v1.11.1
+	golang.org/x/text v0.18.0
 )
 
 require (
@@ -12,8 +14,10 @@ require (
 	github.com/ebitengine/gomobile v0.0.0-20240911145611-4856209ac325 // indirect
 	github.com/ebitengine/hideconsole v1.0.0 // indirect
 	github.com/ebitengine/purego v0.8.0 // indirect
+	github.com/go-text/typesetting v0.2.0 // indirect
 	github.com/jezek/xgb v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/image v0.20.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- gui/tetromino.go
+++ gui/tetromino.go
@@ -1,11 +1,28 @@
 package gui
 
 import (
+	"bytes"
+	"image/color"
+	"log"
+
 	"github.com/Arup3201/tetris/api"
+	"github.com/hajimehoshi/ebiten/examples/resources/fonts"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/text/v2"
 	"golang.org/x/text/language"
 )
+
+var (
+	mplusFaceSource *text.GoTextFaceSource
+)
+
+func init() {
+	s, err := text.NewGoTextFaceSource(bytes.NewReader(fonts.MPlus1pRegular_ttf))
+	if err != nil {
+		log.Fatal(err)
+	}
+	mplusFaceSource = s
+}
 
 type tetrominoGui struct {
 	gameApi    *api.Game
@@ -36,11 +53,14 @@ func (t *tetrominoGui) Update() error {
 func (t *tetrominoGui) Draw(screen *ebiten.Image) {
 	if t.isGameOver {
 		const gameOverText = "Game Over!"
+		op := &text.DrawOptions{}
+		op.ColorScale.ScaleWithColor(color.RGBA{0, 0, 0, 255})
 		f := &text.GoTextFace{
+			Source:    mplusFaceSource,
 			Direction: text.DirectionLeftToRight,
 			Size:      24,
 			Language:  language.English,
 		}
-		text.Draw(screen, gameOverText, f, nil)
+		text.Draw(screen, gameOverText, f, op)
 	}
 }
